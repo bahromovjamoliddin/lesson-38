ac7d20949d37d488cd002daf423d91135a516ff7
baxromovjamoliddin <bahromovjamoliddin0077@gmail.com>
2025-01-17 14:31:57 +0500

    hatolik joylar togirlandi

--- main.js
+++ main.js
@@ -1,26 +1,24 @@
-const parentUL = document.querySelector('.parentUl');
+const parentUL = document.querySelector(".parentUl");
 const li = parentUL.children;
 
-for (const child of li) {
- 
-       child.addEventListener('click',(event)=>{
-        for (const item of li) {
-            item.classList.remove("selected")
-        }
-       child.classList.toggle("selected")
-       if (event.ctrlKey  || event.metaKey) {
-        child.classList.remove('selected')
-       }
-       child.onmousedown = ()=> false;
-    })
-  
-}
+// for (const child of li) {
 
-    
+parentUL.addEventListener("click", (event) => {
+  if (!(event.target.tagName == "LI")) {
+    return;
+  }
+  if (
+    (event.target.classList.contains("selected") && event.ctrlKey) ||
+    event.metKey
+  ) {
+    event.target.classList.remove("selected");
+    return;
+  }
 
+  for (const el of parentUL.children) {
+    el.classList.remove("selected");
+  }
+  event.target.classList.add("selected");
 
-
-
-
-
-
+  parentUL.onmousedown = () => false;
+});
